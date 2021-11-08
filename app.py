15ab2ec2b7b55f7a952a9d1161808a6eab78b283
nuke-droid <33710624+nuke-droid@users.noreply.github.com>
2021-11-08 11:38:23 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, redirect, url_for, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import query
 from datetime import datetime
+import hashlib
+import time
+from os import strerror, system
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///HashPhreak.db'
@@ -16,6 +19,129 @@ class query_hist(db.Model):
 
     def __repr__(self):
         return '<Query %r>' % self.id
+
+
+
+#custom hash class
+class hash:
+    
+    #function which takes in plaintext value as strin str
+    def hashgen(self, str, hashtype):
+        hashtype = int(hashtype)    
+        if hashtype == 1:
+            result = hashlib.sha1(str.encode())
+        elif hashtype == 2:
+            result = hashlib.sha224(str.encode())
+        elif hashtype == 3:
+            result = hashlib.sha256(str.encode())
+        elif hashtype == 4:
+            result = hashlib.sha384(str.encode())
+        elif hashtype == 5:
+            result = hashlib.sha512(str.encode())
+            
+            
+        if result != None:
+            hashvalue = result.hexdigest()
+
+        return hashvalue
+    
+
+    def hashdetect(self, str):
+
+        if (len(str)) == 40:
+            hashtype = 'SHA1'
+        elif (len(str)) == 56:
+            hashtype = 'SHA224'
+        elif (len(str)) == 64:
+            hashtype = 'SHA256'
+        elif (len(str)) == 96:
+            hashtype = 'SHA384'
+        elif (len(str)) == 128:
+            hashtype = 'SHA512'
+        else:
+            hashtype = 'Null'
+
+        return hashtype
+
+class crackhash:
+#Start time is recorded and stored for duration calulation
+   def main(self):
+       
+
+        #List of exceptions is created
+        e = (IndexError)
+
+        #hash object is created as h
+        h = hash()
+
+        #prompt for string user input
+        str = input("Enter string to crack: ")
+        hashType = h.hashdetect(str)
+
+        if hashType != 'Null':
+            print(f'{hashType} hash type detected!')
+        else:
+            print("Hash type not detected or supported. Program aborting.")
+            exit()
+        detectedHash = None
+        if hashType == 'SHA1':
+            detectedHash = 1
+        elif hashType == 'SHA224':
+            detectedHash = 2
+        elif hashType == 'SHA256':
+            detectedHash = 3
+        elif hashType == 'SHA384':
+            detectedHash = 4
+        elif hashType == 'SHA512':
+            detectedHash = 5
+
+        #import text file with list of commond passwords
+        with open('pwd.txt') as f:
+            content = f.readlines()
+
+        #instantiates empty 2 dimensional list
+        painbow = [[], []]
+
+        #loops through text file to separate inputs
+        for i in content:
+            #splits each items by newline
+            j = i.splitlines()
+            
+            #loop stores split items into plaintext and hash into two-dimensional list respectively
+            for k in j:
+
+
+
+                l = h.hashgen(k, detectedHash)
+
+                painbow.append([k, l])
+        #counter serves to provide message in the case that there is no corresponding plaintext value found for the input hash
+        ticker = 0
+
+        for i in range(len(painbow)):
+            try:
+                sub = painbow[i]
+                #ystem('clear')
+
+                
+                #if value is found in hashses stored in memory, corresponding plaintext value is displayed and program exit
+                if sub[1] == str:
+                    print(f"Cracked! Password: {sub[0]} Hash: {sub[1]}")
+                    ticker += 1
+                    endtime = time.time()
+
+                    print("--- %s milliseconds ---" % (time.time() - starttime))
+
+                    exit()
+            
+            except e:
+                pass
+
+        if ticker <= 0:
+            print("No corresponding hash found in database.")
+            endtime = time.time()
+        #Total runtime/duration
+        print(endtime - starttime)
 
 
 @app.route("/", methods=['POST', 'GET'])
